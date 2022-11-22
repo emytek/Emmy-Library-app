854c6d6ee2934ae532383ab704425b56ea9e9bb7
emytek <66952283+emytek@users.noreply.github.com>
2022-11-22 02:00:21 +0100

    Issue

--- server.js
+++ server.js
@@ -12,7 +12,7 @@
   
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
-//   const bookRouter = require('./routes/books')
+const bookRouter = require('./routes/books')
 
   app.set('view engine', 'ejs')
   app.set('views', __dirname + '/views')
@@ -33,6 +33,6 @@ mongoose
   
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
-//   app.use('/books', bookRouter)
+app.use('/books', bookRouter)
   
-  app.listen(process.env.PORT || 8000)
+app.listen(process.env.PORT || 8000)
